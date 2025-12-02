9c557a3d80c95c40621eb6785e18341c399cc382
unknown <dungcoituty06@gmail.com>
2025-12-02 21:11:22 +0700

    Update more pics

--- js/script.js
+++ js/script.js
@@ -6,14 +6,19 @@ document.addEventListener("DOMContentLoaded", function() {
     {src: "../assets/img/sample1.jpg", info: "Sinh vật huyền bí của Wuwa ở confestion"},
     {src: "../assets/img/sample2.jpg", info: "Các người bắt nạt ta"},
     {src: "../assets/img/sample3.jpg", info: "Nàng cáo m5"},
+    {src: "../assets/img/sample4.jpg", info: "Thiếu nữ tai cáo dưới gốc anh đào Yae Miko, nhìn bóng người cô đơn thật đúng là tội.. vơ 🐧"},
+    {src: "../assets/img/sample5.jpg", info: "Doro-sama"},
+    {src: "../assets/img/sample6.jpg", info: "Flop ư, ghi tên chị vào"},
+    {src: "../assets/img/sample7.jpg", info: "Tắm là 1 nghệ thuật"},
+    {src: "../assets/img/sample8.jpg", info: "Ahoyy!"},
+    {src: "../assets/img/sample9.jpg", info: "Thiếu nữ của Tử Vong"},
     // Thêm bao nhiêu ảnh cũng được, mảng sẽ tự mở rộng
   ];
-imagesData.push({src: "../assets/img/sample4.jpg", info: "Thiếu nữ tai cáo dưới gốc anh đào Yae Miko, nhìn bóng người cô đơn thật đúng là tội.. vơ 🐧"});
 
   // Render ảnh ra HTML
   imagesData.forEach(img => {
     const col = document.createElement("div");
-    col.className = "col-md-4 text-center mb-3";
+    col.className = "col-6 col-sm-6 col-md-4 col-lg-3 text-center mb-3";
     col.innerHTML = `
       <img src="${img.src}" 
            class="img-fluid img-thumbnail clickable mx-auto d-block" 
@@ -43,9 +48,19 @@ function changeLayout(columns) {
 
   cols.forEach(col => {
     col.className = "text-center mb-3"; // reset
-    if (columns == 2) col.classList.add("col-md-6");
-    if (columns == 3) col.classList.add("col-md-4");
-    if (columns == 4) col.classList.add("col-md-3");
+
+    if (columns == 2) {
+      // 2 ảnh mỗi hàng trên mọi màn hình
+      col.classList.add("col-6");
+    }
+    if (columns == 3) {
+      // 2 ảnh trên mobile, 3 ảnh trên tablet/desktop
+      col.classList.add("col-6", "col-md-4");
+    }
+    if (columns == 4) {
+      // 2 ảnh trên mobile, 4 ảnh trên desktop
+      col.classList.add("col-6", "col-md-3");
+    }
   });
 }
 
